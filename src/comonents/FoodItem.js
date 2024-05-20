a6fd1a6ecde32cd6f9633506af10376eaf86b618
sohel123146 <63089979+sohel123146@users.noreply.github.com>
2024-05-20 16:01:08 +0530

    Update FoodItem.js

--- src/comonents/FoodItem.js
+++ src/comonents/FoodItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FoodItem = ({ recipe }) => {
   return (
-    <div key={recipe.url}>
+    <div>
       <img className="card-img-top" src={recipe.image} alt="" style={{width:'300px',height:'300px',borderRadius:'10%'}} />
       <div className="card-body">
         <h5 className="card-title">{recipe.label} - {recipe.cuisineType}</h5>
